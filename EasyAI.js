f64b5838ae1507c12af0032037b79418e0949d0d
Daniel Massud <danielmassud@outlook.com>
2023-11-02 05:04:06 -0300

    Método Sleep podendo ser consumido a partir de uma instancia do EasyAI também

--- EasyAI.js
+++ EasyAI.js
@@ -11,12 +11,16 @@ class EasyAI {
 async Generate(prompt = 'Once upon a time',config = {stream : false},tokenCallback = (token) => {}){
     return await this.LlamaCPP.Generate(prompt,config,tokenCallback)
 }
-    
+
+async Sleep(ms) {
+    await EasyAI.Sleep(ms)
+}
+
 static ModelManager = {
 
 }
 
-static Sleep = (ms) => {
+static Sleep = async (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
