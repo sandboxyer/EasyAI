fd821b6fae2f18321b773d49132f8d9667b27c13
Daniel Massud <danielmassud@outlook.com>
2023-11-21 06:01:08 -0300

    Criada a interface TerminalHUD que possibilitaa criação de uma UI de navegação no terminal

---
+++ core/TerminalHUD.js
@@ -0,0 +1,92 @@
+import readline from 'readline';
+
+class TerminalHUD {
+  constructor() {
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+  }
+
+  ask(question) {
+    return new Promise(resolve => {
+      this.rl.question(question, answer => {
+        resolve(answer);
+      });
+    });
+  }
+
+  async displayMenu(menuGenerator, clearScreen = false) {
+    if (clearScreen) {
+      console.clear();
+    }
+
+    const menu = menuGenerator(); // Generate menu using external state
+
+    console.log(menu.title);
+    menu.options.forEach((option, index) => {
+      console.log(`${index + 1}. ${option.name}`);
+    });
+
+    const choice = parseInt(await this.ask('Choose an option: '));
+    const chosenOption = menu.options[choice - 1];
+
+    if (chosenOption) {
+      await chosenOption.action();
+    } else {
+      console.log('Invalid option, try again.');
+      await this.displayMenu(menuGenerator, clearScreen);
+    }
+  }
+
+  close() {
+    this.rl.close();
+  }
+}
+
+export default TerminalHUD
+
+// Use Example \/
+/*
+// External variable for dynamic menu content
+let dynamicOption = 'cocozao';
+
+let dinamicao = 'menuzao'
+
+// Menu generation functions
+const createSubMenu1 = () => ({
+  title: dinamicao,
+  options: [
+    {
+      name: 'Collect Data',
+      action: async () => {
+        const userData = await terminalHUD.ask('Please enter some data: ');
+        dinamicao = userData;
+        await terminalHUD.displayMenu(createSubMenu1, true);
+      }
+    },
+    {
+      name: dynamicOption,
+      action: async () => {
+        console.log(`Selected: ${dynamicOption}`);
+        await terminalHUD.displayMenu(createSubMenu1, true);
+      }
+    },
+    {
+      name: 'Back to Main Menu',
+      action: () => terminalHUD.displayMenu(createMainMenu, true)
+    }
+  ]
+});
+
+const createMainMenu = () => ({
+  title: 'Main Menu',
+  options: [
+    { name: 'Submenu 1', action: () => terminalHUD.displayMenu(createSubMenu1, true) },
+    { name: 'Exit', action: () => terminalHUD.close() }
+  ]
+});
+
+const terminalHUD = new TerminalHUD();
+terminalHUD.displayMenu(createMainMenu, true);
+*/
