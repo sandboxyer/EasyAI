2629c301b92638e7949c58e72affa50c691a16a6
Daniel Massud <danielmassud@outlook.com>
2024-02-21 12:16:05 -0300

    Porta configuravel no sandboxconfig.js

--- core/MenuCLI/Sandbox/SandboxConfig.js
+++ core/MenuCLI/Sandbox/SandboxConfig.js
@@ -7,7 +7,7 @@ let instance_config = {server_url : 'localhost'}
 const SandboxConfig = () => ({
     title : `• Sandbox Config •
 
-URL : ${instance_config.server_url}
+URL : ${instance_config.server_url}${(instance_config.server_port) ? `:${instance_config.server_port}` : ''}
 `,
 options : [
     {
@@ -18,12 +18,18 @@ options : [
         },
     {
     name : `Alterar URL`,
-    action : () => {
+    action : async () => {
+        let newurl = await MenuCLI.ask('Novo URL : ')
+        instance_config.server_url = newurl
+        MenuCLI.displayMenu(SandboxConfig)
     }
     },
     {
     name : 'Configurar Porta',
-    action : () => {
+    action : async () => {
+        let newport = await MenuCLI.ask('Nova Porta : ')
+        instance_config.server_port = newport
+        MenuCLI.displayMenu(SandboxConfig)
     }
     },
     {
