bf255c66e5b59a0eecfb561e304a272f26172a45
Daniel Massud <danielmassud@outlook.com>
2024-04-28 21:33:33 -0300

    Removendo import do getTime no Message.js, trazendo a function para dentro

--- core/ChatModule/Message.js
+++ core/ChatModule/Message.js
@@ -1,4 +1,13 @@
-import getTime from '../useful/getTime.js'
+function getTime() {
+    let now = new Date();
+    let day = String(now.getDate()).padStart(2, '0');
+    let month = String(now.getMonth() + 1).padStart(2, '0'); // January is 0!
+    let year = now.getFullYear();
+    let hours = String(now.getHours()).padStart(2, '0');
+    let minutes = String(now.getMinutes()).padStart(2, '0');
+
+    return `${day}/${month}/${year} ${hours}:${minutes}`;
+}
 
 class Message {
     constructor(sender,content,config = {id : undefined,time : false}){
