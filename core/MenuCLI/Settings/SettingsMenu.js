6f37171afff24cf6421a518efa64859e44814dbf
Daniel Massud <danielmassud@outlook.com>
2024-05-09 21:20:22 -0300

    Crada class utilitaria ColorText() em /useful e criada a opção de configurar o start com o CUDA por padrão

--- core/MenuCLI/Settings/SettingsMenu.js
+++ core/MenuCLI/Settings/SettingsMenu.js
@@ -1,17 +1,25 @@
 import MenuCLI from "../MenuCLI.js"
 import StartMenu from "../StartMenu.js"
+import ConfigManager from './ConfigManager.js'
+import ColorText from '../../useful/ColorText.js'
 
 const SettingsMenu = () => ({
     title : `⚙️ Centos Requirements
 `,
 options : [
     {
-    name : 'Start w/CUDA',
+    name : `Start w/CUDA | ${(ConfigManager.getKey('start-cuda') ? ColorText.green('ON') : ColorText.red('OFF'))}`,
     action : () => {
+        if(ConfigManager.getKey('start-cuda')){
+            ConfigManager.setKey('start-cuda',false)
+        } else {
+            ConfigManager.setKey('start-cuda',true)
+        }
+        MenuCLI.displayMenu(SettingsMenu)
     }
     },
     {
-    name : 'OpenAI Token',
+    name : 'OpenAI',
     action : () => {
         }
     },
---
+++ core/useful/ColorText.js
@@ -0,0 +1,31 @@
+class ColorText {
+    static red(text) {
+        return `\x1b[31m${text}\x1b[0m`; // Red text
+    }
+
+    static green(text) {
+        return `\x1b[32m${text}\x1b[0m`; // Green text
+    }
+
+    static yellow(text) {
+        return `\x1b[33m${text}\x1b[0m`; // Yellow text
+    }
+
+    static blue(text) {
+        return `\x1b[34m${text}\x1b[0m`; // Blue text
+    }
+
+    static magenta(text) {
+        return `\x1b[35m${text}\x1b[0m`; // Magenta text
+    }
+
+    static cyan(text) {
+        return `\x1b[36m${text}\x1b[0m`; // Cyan text
+    }
+
+    static white(text) {
+        return `\x1b[37m${text}\x1b[0m`; // White text
+    }
+}
+
+export default ColorText
