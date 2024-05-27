cd01fc9562f151f90c96f812183026d7bcf25408
Daniel Massud <danielmassud@outlook.com>
2024-05-27 12:38:57 -0300

    Incluido o inicio do EasyAI server com PM2 no flash command ai `ai save_name`

--- core/MenuCLI/MenuCLI.js
+++ core/MenuCLI/MenuCLI.js
@@ -15,9 +15,17 @@ const args = process.argv.slice(2);
 if (args.length > 0) {
     
     await ServerSaves.Load(args[0])
-    .then(save => {
-        let server = new EasyAI.Server({token : save.Token,port : save.Port,EasyAI_Config : save.EasyAI_Config})
-        server.start()
+    .then(async (save) => {
+        if(save.PM2){
+            await EasyAI.Server.PM2({token : save.Token,port : save.Port,EasyAI_Config : save.EasyAI_Config})
+            console.log('✔️ PM2 Server iniciado com sucesso !')
+            process.exit()
+        } else {
+            let server = new EasyAI.Server({token : save.Token,port : save.Port,EasyAI_Config : save.EasyAI_Config})
+            server.start()
+        }
+        
+        
     }).catch(e => {
         console.log(`Save ${ColorText.red(args[0])} não foi encontrado`)
         process.exit()
