8a05ad5f7f2b050cd6d18c4e612a7d3b6d07c576
Daniel Massud <danielmassud@outlook.com>
2024-06-15 15:25:15 -0300

    Implementação do load e set do openai token e model no ChatFlash.js

--- core/Flash/ChatFlash.js
+++ core/Flash/ChatFlash.js
@@ -8,26 +8,43 @@ import PM2 from "../useful/PM2.js"
 import ServerSaves from "../MenuCLI/ServerSaves.js"
 import ConfigManager from "../ConfigManager.js"
 import ColorText from '../useful/ColorText.js'
+import TerminalHUD from "../TerminalHUD.js"
 
 let ai
 
 const args = process.argv.slice(2);
 
 if (args.length > 0 || ConfigManager.getKey('defaultchatsave')){
-    await ServerSaves.Load((args.length > 0) ? args[0] : ConfigManager.getKey('defaultchatsave'))
-    .then(async (save) => {
+    let toload = (args.length > 0) ? args[0] : ConfigManager.getKey('defaultchatsave')
+    if(toload.toLowerCase == 'openai'){
+        if(ConfigManager.getKey('openai')){
+            let openai_info = ConfigManager.getKey('openai')
+            ai = new EasyAI({openai_token : openai_info.token, openai_model  })
+        } else {
+            let cli = new TerminalHUD()
+            let final_object = {}
+            final_object.token = await cli.ask('OpenAI Token : ')
+            final_object.model = await cli.ask('Select the model',{options : ['gpt-3.5-turbo','gpt-4','gpt-4-turbo-preview','gpt-3.5-turbo-instruct']})
 
-            await EasyAI.Server.PM2({token : save.Token,port : save.Port,EasyAI_Config : save.EasyAI_Config})
-            console.log('✔️ PM2 Server iniciado com sucesso !')
+            ConfigManager.setKey('openai',final_object)
+        }
+    } else {
+        await ServerSaves.Load(toload)
+        .then(async (save) => {
+    
+                await EasyAI.Server.PM2({token : save.Token,port : save.Port,EasyAI_Config : save.EasyAI_Config})
+                console.log('✔️ PM2 Server iniciado com sucesso !')
+                ai = new EasyAI({server_url : 'localhost',server_port : 4000})
+    
+        }).catch(async e => {
+    
+            console.log(`Save ${ColorText.red(args[0])} não foi encontrado`)
+            await EasyAI.Server.PM2()
             ai = new EasyAI({server_url : 'localhost',server_port : 4000})
-
-    }).catch(async e => {
-
-        console.log(`Save ${ColorText.red(args[0])} não foi encontrado`)
-        await EasyAI.Server.PM2()
-        ai = new EasyAI({server_url : 'localhost',server_port : 4000})
+       
+        })
+    }
    
-    })
 } else {
     await EasyAI.Server.PM2()
     ai = new EasyAI({server_url : 'localhost',server_port : 4000})
